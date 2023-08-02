832f9665141c92701634cf217fbb22eca13dec27
Ramesh Kumar <rameshkumar455555@gmailcom>
2023-08-02 23:52:18 +0530

    Day_11

---
+++ Day_11/Sliding_Window_Maximum.cpp
@@ -0,0 +1,36 @@
+/*
+problem:- https://leetcode.com/problems/sliding-window-maximum/
+*/
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        deque <int> deq;
+        vector <int> res;
+
+        for(int i = 0; i < k; i++){
+            while(!deq.empty() && nums[i] > nums[deq.back()]){
+                deq.pop_back();
+            }
+
+            deq.push_back(i);
+        }
+        res.push_back(nums[deq.front()]);
+
+        for(int i = k; i < n; i++){
+            while(!deq.empty() && nums[i] > nums[deq.back()]){
+                deq.pop_back();
+            }
+
+            deq.push_back(i);
+            if(deq.front() == i - k)
+            {
+                deq.pop_front();
+            } 
+            res.push_back(nums[deq.front()]);
+        }
+
+        return res;
+    }
+};
