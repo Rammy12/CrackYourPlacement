68bb0e53e380433e33ac4466588970ff2e8f3fc3
Ramesh Kumar <rameshkumar455555@gmailcom>
2023-08-03 17:41:22 +0530

    Day_12

---
+++ Day_12/Sort_Colors.cpp
@@ -0,0 +1,44 @@
+/*
+probelm:- https://leetcode.com/problems/sort-colors/
+*/
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        int zeroCount=0;
+        int oneCount=0;
+        int twoCount=0;
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i]==0)
+            {
+                zeroCount++;
+            }
+            else if(nums[i]==1)
+            {
+                oneCount++;
+            }
+            else
+            {
+                twoCount++;
+            }
+        }
+        int j=0;
+        while(zeroCount)
+        {
+            nums[j++]=0;
+            zeroCount--;
+        }
+        while(oneCount)
+        {
+            nums[j++]=1;
+            oneCount--;
+        }
+        while(twoCount)
+        {
+            nums[j++]=2;
+            twoCount--;
+        }
+    }
+};
