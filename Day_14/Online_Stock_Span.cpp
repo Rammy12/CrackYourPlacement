4a8b6a1dc0306527ab7ddbea32c65b9040f30001
Ramesh Kumar <rameshkumar455555@gmailcom>
2023-08-05 23:28:38 +0530

    Day_14

---
+++ Day_14/Online_Stock_Span.cpp
@@ -0,0 +1,28 @@
+/*
+problem:- https://leetcode.com/problems/online-stock-span/
+*/
+
+lass StockSpanner {
+public:
+    stack<pair<int,int>>st;
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        int result=1;
+        while(!st.empty() and st.top().first<=price)
+        {
+            result+=st.top().second;
+            st.pop();
+        }
+        st.push({price,result});
+        return result;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
